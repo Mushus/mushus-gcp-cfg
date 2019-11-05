a1be6d45f1870370f17b39e465677acb35c09fb9
Mushus <Mushus.wynd@gmail.com>
2019-11-05 20:55:45 +0900

    update dummy settings

--- web/provision.sh
+++ web/provision.sh
@@ -12,6 +12,7 @@ CERTBOT_CMD=certbot-auto
 
 mkdir -p /var/www/sagyoipe.mushus.net
 mkdir -p /var/log/sagyoipe
+mkdir -p /etc/letsencrypt/live/sagyoipe.mushus.net/
 
 #
 ## Install nginx
@@ -49,7 +50,12 @@ rm -rf /etc/nginx/sites-available/*
 rm -rf /etc/nginx/sites-enabled/*
 
 cp ${SCRIPT_DIR}/etc/nginx/sites-available/* /etc/nginx/sites-available
-ls /etc/nginx/sites-available | xargs -I{} ln -s /etc/nginx/sites-available/{} /etc/nginx/sites-enabled/{}
+
+if [ -e /etc/letsencrypt/live/sagyoipe.mushus.net/fullchain.pem ]; then
+  ln -s /etc/nginx/sites-available/dummy.sagyoipe.mushus.net /etc/nginx/sites-enabled/dummy.sagyoipe.mushus.net
+else
+  ln -s /etc/nginx/sites-available/sagyoipe.mushus.net /etc/nginx/sites-enabled/sagyoipe.mushus.net
+fi
 
 cp -f ${SCRIPT_DIR}/etc/nginx/nginx.conf /etc/nginx/nginx.conf
 
@@ -69,4 +75,19 @@ if !(type $CERTBOT_CMD > /dev/null 2>&1); then
     --webroot \
     --webroot-path /var/www/sagyoipe.mushus.net \
     --domain sagyoipe.mushus.net
+fi
+
+#
+## Update nginx config file
+#
+
+NEED_RESTART=false
+if [ -e /etc/nginx/sites-enabled/dummy.sagyoipe.mushus.net ]; then
+  unlink /etc/nginx/sites-available/dummy.sagyoipe.mushus.net /etc/nginx/sites-enabled/dummy.sagyoipe.mushus.net
+  ln -s /etc/nginx/sites-available/sagyoipe.mushus.net /etc/nginx/sites-enabled/sagyoipe.mushus.net
+  NEED_RESTART=true
+fi
+
+if "${NEED_RESTART}"; then
+  service nginx restart
 fi
