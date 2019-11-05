653fef2aa854089ab01b8e86a86e75aeb457c1d6
Mushus <Mushus.wynd@gmail.com>
2019-11-05 21:43:30 +0900

    update

--- web/provision.sh
+++ web/provision.sh
@@ -63,7 +63,7 @@ cp ${SCRIPT_DIR}/etc/nginx/sites-available/* /etc/nginx/sites-available
 if [ -e /etc/letsencrypt/live/sagyoipe.mushus.net/fullchain.pem ]; then
   ln -s /etc/nginx/sites-available/sagyoipe.mushus.net /etc/nginx/sites-enabled/sagyoipe.mushus.net
 else
-  ln -s /etc/nginx/sites-available/dummy.sagyoipe.mushus.net /etc/nginx/sites-enabled/dummy.sagyoipe.mushus.net
+  ln -s /etc/nginx/sites-available/nonssl.sagyoipe.mushus.net /etc/nginx/sites-enabled/dummy.sagyoipe.mushus.net
 fi
 
 cp -f ${SCRIPT_DIR}/etc/nginx/nginx.conf /etc/nginx/nginx.conf
